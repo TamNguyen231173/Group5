8ba8f0e3485b42e7d6e1746565ebde398813820c
tu276fnnn <qtuvip@gmail.com>
2023-05-31 12:52:21 +0700

    feat: code xong UI  fgpass & newpass & verificatin

--- app/src/screens/Forgotpassword/ForgotpasswordScreen.tsx
+++ app/src/screens/Forgotpassword/ForgotpasswordScreen.tsx
@@ -8,34 +8,41 @@ export const ForgotpasswordScreen = () => {
 
     return (
         <Container>
-            <StatusBar backgroundColor={'#fff'} hidden={true}/>
-            <Block flex backgroundColor="#fff" >
-                <Text size={22} lineHeight={32} color='#333647' fontFamily='bold' paddingLeft={24}>
-                    Forgot Password 🤔
-                </Text>
-                <Text size={16} lineHeight={24} color='#000000' fontFamily='medium' paddingLeft={24} paddingTop={7}>
-                    Bạn cần nhập email để nhận mã code
-                </Text>
-                <Block alignCenter justifyCenter paddingTop={52}>
-                    <TextInputApp 
-                        placeholder='Email Adress'
-                        onChangeText={(text) => {
-                            setemail(text);
-                        }}
-                        value={email}
-                        type='email' /></Block>
-                <Block alignCenter justifyCenter paddingTop={131}>
+            <StatusBar backgroundColor={'#fff'}/>
+            <Block flex backgroundColor="#fff" space='between'>
+                <Block>
+                    <Block paddingTop={27}>
+                    <Text size={22} lineHeight={32} color='#333647' fontFamily='bold' paddingLeft={24}>
+                        Forgot Password 🤔
+                    </Text>
+                    <Text size={16} lineHeight={24} color='#000000' fontFamily='medium' paddingLeft={24} paddingTop={7}>
+                        Bạn cần nhập email để nhận mã code
+                    </Text>
+                    </Block>
+                    <Block alignCenter justifyCenter paddingTop={52}>
+                        <TextInputApp
+                            placeholder='Email Adress'
+                            onChangeText={(text) => {
+                                setemail(text);
+                            }}
+                            value={email}
+                            type='email' />
+                    </Block>
+                </Block>
+                <Block alignCenter justifyCenter>
                     <ButtonApp
-                        type={'outline'}
+
+                        type='outline'
                         title={'Next'}
                         onClick={() => {
                             console.log('Check')
                         }}
                     /></Block>
-                <Block absolute bottom={48}>
+                <Block marginBottom={48}>
                     <Text size={16} lineHeight={18} color='#7C82A1' fontFamily='medium' paddingLeft={54}>
                         Remember the password?<Text color='#333647'>Try again</Text>
-                    </Text></Block>
+                    </Text>
+                </Block>
             </Block>
         </Container>
     )

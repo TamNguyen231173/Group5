9f36aa0cd945c391876a5730d3732516f9228718
josephanh <josephanh129@gmail.com>
2023-06-02 11:25:53 +0700

    feat: search screen

--- app/src/assets/icons/SearchIcon.tsx
+++ app/src/assets/icons/SearchIcon.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import Svg, { SvgProps, Circle, Path } from 'react-native-svg'
-export const SearchIcon = (props: SvgProps) => (
-  <Svg width={24} height={24} fill="none" {...props}>
-    <Circle cx={11} cy={11} r={6} stroke="#fff" strokeWidth={2} />
-    <Path stroke="#fff" strokeLinecap="round" strokeWidth={2} d="m20 20-3-3" />
+
+interface Props extends SvgProps {
+  fill?: string;
+}
+export const SearchIcon = ({fill = "#fff"}:Props) => (
+  <Svg width={24} height={24} fill="none">
+    <Circle cx={11} cy={11} r={6} stroke={fill} strokeWidth={2} />
+    <Path stroke={fill} strokeLinecap="round" strokeWidth={2} d="m20 20-3-3" />
   </Svg>
 )

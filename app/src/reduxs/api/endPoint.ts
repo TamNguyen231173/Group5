2c9fec5517061e0caf453df377a89b599f515604
TranVux <vutran789jjjj@gmail.com>
2023-06-08 18:15:02 +0700

    fix: update endPoint in api folder

--- app/src/reduxs/api/endPoint.ts
+++ app/src/reduxs/api/endPoint.ts
@@ -1,19 +1,19 @@
-export enum EndPoint {
+export class EndPoint {
   // login
-  login = '/api/auth/login',
+  static login = '/api/auth/login'
 
   // resgister
-  register = '/api/auth/register',
+  static register = '/api/auth/register'
 
   // post
-  getAllPost = '/api/posts',
-  getPostById = '/api/posts/:id',
+  static getAllPost = '/api/posts'
+  static getPostById = (id: string) => `/api/posts/${id}`
 
   // category
-  getAllCategory = '/api/categories',
-  getCategoryById = '/api/categories/:id',
+  static getAllCategory = '/api/categories'
+  static getCategoryById = (id: string) => `/api/categories/${id}`
 
   // video
-  getAllVideo = '/api/videos',
-  getVideoById = '/api/videos/:id',
+  static getAllVideo = '/api/videos'
+  static getVideoById = (id: string) => `/api/videos/${id}`
 }
